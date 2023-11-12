89f954a58685ae490c46b238836d72201c06642a
Anyaalh <anya.alh@gmail.com>
2023-11-13 02:04:07 +0300

    map

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -32,7 +32,7 @@ export const Todolist = (props: TodolistPropsType) => {
             {/*<li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>*/}
         </ul>
         <div>
-            <button>All</button>
+            <button>All!</button>
             <button>Active</button>
             <button>Completed</button>
         </div>
